e505447e233295f24e1094550be295c1298636af
pVnRT <pVnRT@cock.li>
2018-04-24 20:22:33 +0200

    Add KQueue(MIN) implementation

---
+++ KQueue/KQueue.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+template <class T>
+struct KQueue {
+    deque<pair<int, T>> DQ;
+
+    void push(T val) {
+        int q = 0;
+        while(!DQ.empty() && DQ.back().first >= val)
+            q += DQ.back().second + 1,
+            DQ.pop_back();
+        DQ.push_back({val, q});
+    }
+
+    void pop() {
+        if(DQ.front().second == 0)
+            DQ.pop_front();
+        else
+            --DQ.front().second;
+    }
+
+    inline T min() {
+        return DQ.front().first;
+    }
+};
+
+int main() {
+    return 0;
+}
